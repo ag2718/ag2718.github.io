268eb7391285516e1415b530d25968e278b1c7f9
ag2718 <51494575+ag2718@users.noreply.github.com>
2025-07-08 05:13:29 +0000

    Deploying to gh-pages from @ ag2718/ag2718.github.io@280559e31f85711519d0681a2c29abc0e92db566 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,13 +44,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
         },{id: "nav-people",
           title: "people",
           description: "members of the lab or group",
@@ -508,7 +501,7 @@ ninja.data = [{
         title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.linkedin.com/in/aarushg", "_blank");
+          window.open("https://www.linkedin.com/in/aarush-g", "_blank");
         },
       },{
         id: 'social-rss',
@@ -523,13 +516,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
